58d15a536382feedea7107c5f276f6e496777fd6
Julien Caselmann <julien.caselmann@tum.de>
2019-11-21 15:41:30 +0100

    Kuensche Flaeche solidified and linspace with doubled amounts of points for better resolution. ready to print

--- src/main_script.m
+++ src/main_script.m
@@ -4,8 +4,8 @@ funx = @(u,v) (2.*cosh(u).*(cos(v) + v.*sin(v)))./(v.*v+cosh(u).*cosh(u));
 funy = @(u,v) (2.*cosh(u) .* (sin(v) - v.*cos(v))) ./ (v.*v + cosh(u) .* cosh(u));
 funz = @(u,v) u - (sinh(2.*u)) ./ (v.*v + cosh(u) .* cosh(u));
 
-u = linspace(-2*pi,2*pi) ; 
-v = linspace(-2*pi,2*pi) ;
+u = linspace(-2*pi,2*pi,200) ; 
+v = linspace(-2*pi,2*pi,200) ;
 
 [U, V] = meshgrid(u,v);
 x = (2.*cosh(U).*(cos(V) + V.*sin(V)))./(V.*V+cosh(U).*cosh(U));
@@ -14,6 +14,6 @@ z = U - (sinh(2.*U)) ./ (V.*V + cosh(U) .* cosh(U));
 
 surf(x,y,z)
 
-saveobjmesh('Kuensche_flaeche.obj',x,y,z);
+saveobjmesh('Kuensche_flaeche_2.obj',x,y,z);
 
 
